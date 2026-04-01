7ff9f9545df2d4a407a049718faa997ab53a848f
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-04-01 11:04:13 -0400

    package app

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,16 +3,18 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q PACKAGENAME | awk '{print $2; exit}') # example command to get version of application here
+VERSION=$(pacman -Q pdftricks | awk '{print $2; exit}') # example command to get version of application here
 export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=PATH_OR_URL_TO_ICON
-export DESKTOP=PATH_OR_URL_TO_DESKTOP_ENTRY
+export ICON=/usr/share/icons/hicolor/256x256/apps/com.github.muriloventuroso.pdftricks.svg
+export DESKTOP=/usr/share/applications/com.github.muriloventuroso.pdftricks.desktop
+export DEPLOY_IMAGEMAGICK=1
+export DEPLOY_GHOSTSCRIPT=1
 
 # Deploy dependencies
-quick-sharun /PATH/TO/BINARY_AND_LIBRARIES_HERE
+quick-sharun /usr/bin/pdftricks
 
 # Additional changes can be done in between here
 
